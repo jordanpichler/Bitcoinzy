60fdc04f93fa2a4dddd357bb2ab0befb8e957f06
Jordan Pichler <j.pichler@icloud.com>
2017-04-09 00:58:14 +0200

    - JSON communication with bitcoinaverage.com

--- Bitquinzy/FirstViewController.swift
+++ Bitquinzy/FirstViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var rateDisplayLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        retrieveBTCExchangeRate(for: "USD")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func retrieveBTCExchangeRate(for currency: String) {
+        self.rateDisplayLabel.text =  "updating..."
+
+        let url = "https://apiv2.bitcoinaverage.com/indices/local/ticker/BTC\(currency)"
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                
+                // always represent rate with 2 decimals
+                let rate = json["ask"].floatValue
+                self.rateDisplayLabel.text =  String(format: "%.2f", arguments: [rate])
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
-
 }
 
---
+++ Pods/Target Support Files/Pods-Bitcoinzy/Pods-Bitcoinzy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bitcoinzy : NSObject
+@end
+@implementation PodsDummy_Pods_Bitcoinzy
+@end
