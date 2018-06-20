75d12b08ece252b5abc845d6426b1a6b0dfe69ba
Jordan Pichler <j.pichler@icloud.com>
2018-06-20 21:23:40 +0200

    Define ValueContainerView class

---
+++ Bitquinzy/View/ValueContainerView.swift
@@ -0,0 +1,38 @@
+//
+//  ValueContainerView.swift
+//  Bitcoinzy
+//
+//  Created by Jordan Pichler on 20/06/2018.
+//  Copyright © 2018 Jordan A. Pichler. All rights reserved.
+//
+
+import UIKit
+
+/**
+ View containing values (e.g current rate, day's high and low, volume, ...)
+ of the current state of the BTC.
+ */
+class ValueContainerView: UIView {
+    
+    // MARK: - Properties -
+    
+    var mainValue: ValueView!
+    var subValues: [ValueView]!
+    
+    // MARK: - Initializers -
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented. Still got no IB?")
+    }
+    
+    convenience init(mainValue: ValueView, subValues: [ValueView]) {
+        self.init()
+    }
+
+    
+    
+}
